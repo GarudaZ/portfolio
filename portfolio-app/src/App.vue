0f6e013ef2e4d622f03c503c3f1feef841c2548d
Gareth A <garethjaustin@gmail.com>
2024-06-21 21:53:37 +0100

    adds basic routing

--- portfolio-app/src/App.vue
+++ portfolio-app/src/App.vue
@@ -1,19 +1,31 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+<script setup>
+import { ref, computed } from "vue";
 import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+import Projects from "./components/Projects.vue";
+import NotFound from "./components/NotFound.vue";
+
+const routes = {
+	"/": Home,
+	"/about": About,
+	"/projects": Projects,
+};
+
+const currentPath = ref(window.location.hash);
+
+window.addEventListener("hashchange", () => {
+	currentPath.value = window.location.hash;
+});
+
+const currentView = computed(() => {
+	return routes[currentPath.value.slice(1) || "/"] || NotFound;
+});
 </script>
 
 <template>
-	<div>
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
-	<HelloWorld msg="Vite + Vue" />
-	<Home />
+	<a href="#/">Home</a> | <a href="#/about">About</a> |
+	<a href="#/projects">Projects</a>
+	<component :is="currentView" />
 </template>
 
 <style scoped>
