22f9fe8bc2843c2ffb5b6b0c0df256ddf8497e58
Gareth A <garethjaustin@gmail.com>
2024-07-10 16:34:48 +0100

    adds dracula color scheme and color shift

--- portfolio-app/src/components/WordRotator.vue
+++ portfolio-app/src/components/WordRotator.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="rotating-word">
-		<p>{{ displayedWord }}</p>
+		<p :style="{ color: currentColour }">{{ displayedWord }}</p>
 	</div>
 </template>
 
@@ -14,12 +14,17 @@ export default {
 				"Frontend Developer",
 				"Backend Developer",
 			],
+			colours: ["#ff5555", "#8be9fd", "#ffb86c", "#ff79c6", "#bd93f9"],
 			currentWordIndex: 0,
+			currentColourIndex: 0,
 		};
 	},
 	computed: {
 		displayedWord() {
 			return this.words[this.currentWordIndex];
+		},
+		currentColour() {
+			return this.colours[this.currentColourIndex];
 		},
 	},
 	mounted() {
@@ -29,6 +34,8 @@ export default {
 		startRotation() {
 			setInterval(() => {
 				this.currentWordIndex = (this.currentWordIndex + 1) % this.words.length;
+				this.currentColourIndex =
+					(this.currentColourIndex + 1) % this.colours.length;
 			}, 2000);
 		},
 	},
