1f2cbad5cd62ac8015b5974f27a35044ee06e3a7
Gareth A <garethjaustin@gmail.com>
2024-07-11 13:55:29 +0100

    adds further styling and intro text

--- portfolio-app/src/components/Home.vue
+++ portfolio-app/src/components/Home.vue
@@ -1,8 +1,44 @@
 <template>
-	<div class="welcome">
-		<p>Hi</p>
-		<p>I'm Gareth Austin</p>
-		<RotatingWords />
+	<div class="container">
+		<div class="welcome">
+			<p>Hi</p>
+			<p>
+				I'm
+
+				<span style="font-weight: 800">Gareth Austin</span>
+			</p>
+			<RotatingWords />
+		</div>
+
+		<article style="font-size: 16pt">
+			<p>
+				Having discovered I had a taste for software development in my last
+				role, I completed automation projects using Google Apps Script that
+				saved the charity hours of work each week. The creativity involved in
+				imagining improvements, and the problem solving and the learning needed
+				to implement them really motivated my move to the tech sector. I have
+				just completed a software development course with Northcoders and I am
+				looking for my first junior development role.
+			</p>
+			<p>
+				An experienced public speaker with a love of the English Language, I
+				have a masters in English Literature and, as an ordained Buddhist, I
+				teach meditation and Buddhism at my local centre. I'm open for work so
+				feel free to get in touch.
+			</p>
+			<p>
+				I love learning new technology and you can take a look at what I've been
+				<a href="#/projects">working on here.</a>
+			</p>
+			<!-- <a [routerLink]="[ '/projects', routeParam ]">here</a> -->
+			<!-- <a :to="{ path: '(/projects)' }">here</a> -->
+		</article>
+		<aside>
+			<p style="font-size: 18pt">
+				Get in touch:
+				<a href="mailto: garethjaustin@gmail.com">garethjaustin@gmail.com </a>
+			</p>
+		</aside>
 	</div>
 </template>
 
--- portfolio-app/src/components/WordRotator.vue
+++ portfolio-app/src/components/WordRotator.vue
@@ -13,6 +13,7 @@ export default {
 				"Software Engineer",
 				"Frontend Developer",
 				"Backend Developer",
+				"JavaScript Developer",
 			],
 			colours: ["#ff5555", "#8be9fd", "#ffb86c", "#ff79c6", "#bd93f9"],
 			currentWordIndex: 0,
