18498d8013a53d45d4967bc50cc2103a4beadedf
Gareth A <garethjaustin@gmail.com>
2024-07-17 17:21:47 +0100

    adds extra link to backend repo where the project has one

--- portfolio-app/src/components/Projects.vue
+++ portfolio-app/src/components/Projects.vue
@@ -10,16 +10,16 @@ const projects = ref([
 		github: "https://github.com/GarudaZ/portfolio",
 		hosted: "You're here now!",
 		wip: true,
-		tech: ["Vue","CSS","Vite", "Typescript"]
+		tech: ["Vue", "CSS", "Vite", "Typescript"],
 	},
 	{
 		name: "Tetriplan",
 		screenshot: "tetriplan-screenshot.png",
-		desc: "A time blocking app made using Angular and MondoDB",
+		desc: "A time blocking app made using Angular and MongoDB",
 		github: "https://github.com/GarudaZ/tetriplan-frontend",
 		hosted: "https://tetriplan.netlify.app",
 		wip: false,
-		tech: ["Angular", "mongoDB", "Firebase"]
+		tech: ["Angular", "mongoDB", "Firebase"],
 	},
 	{
 		name: "NC News",
@@ -28,50 +28,72 @@ const projects = ref([
 		github: "https://github.com/GarudaZ/fe-nc-news",
 		beGithub: "https://github.com/GarudaZ/nc-news",
 		hosted: "https://ncnewsnow.netlify.app/",
-		wip:false,
-		tech: ["React", "Express", "Javascript"]
+		wip: false,
+		tech: ["React", "Express", "Javascript"],
 	},
 ]);
 </script>
 
 <template>
-	
 	<h1>Projects</h1>
 	<div class="container">
 		<div class="overview">
-
-			<p>Here are my coding projects, past and present. You can also see what technology and skills were involved in each of them.</p>
+			<p>
+				Here are my coding projects, past and present. You can also see what
+				technology and skills were involved in each of them.
+			</p>
 		</div>
-	<div class="cards-container">
-
-		<li v-for="project in projects" class="card">
+		<!-- <div> -->
+		<ul class="cards-container">
+			<li v-for="project in projects" :key="project.name" class="card">
 				<div class="card-header">
-					<h2>{{ project.name }}</h2>
+					<a target="_blank" :href="project.hosted">
+						<h2>{{ project.name }}</h2>
+					</a>
 					<div class="card-header-right">
-						<div v-if="project.wip" class="tag" style="background-color: #8BE9FD;">
-							<p >IN PROGRESS</p>
+						<div
+							v-if="project.wip"
+							class="tag"
+							style="background-color: #8be9fd"
+						>
+							<p>IN PROGRESS</p>
 						</div>
-						<a target="_blank" :href="project.github"><img style="width: 40px; margin-left: 1em;" src="/github-logo.png" alt="github logo"/></a>
+						<a target="_blank" :href="project.github">
+							<img
+								style="width: 40px; margin-left: 1em"
+								src="/github-logo.png"
+								alt="github logo"
+							/>
+						</a>
 					</div>
 				</div>
-			<a target="_blank" :href="project.hosted">
-				<img :src="project.screenshot" alt="Screenshot of the project" />
-			</a>
-			<div class="tech-tags">
-				<div v-for="tech in project.tech" class="tag">
-					<p>{{ tech }}</p>
+				<a target="_blank" :href="project.hosted">
+					<img :src="project.screenshot" alt="Screenshot of the project" />
+				</a>
+				<div class="tech-tags">
+					<div v-for="tech in project.tech" :key="tech" class="tag">
+						<p>{{ tech }}</p>
+					</div>
 				</div>
-			</div>
-			<p>{{ project.desc }}</p>
-			<li style="padding: 0;">
-				<p>GitHub: <a :href="project.github"> {{ project.github }}</a>
-				</p>
+				<p>{{ project.desc }}</p>
+				<div style="padding: 0">
+					<p>
+						GitHub: <a :href="project.github">{{ project.github }}</a>
+					</p>
+				</div>
+				<div v-if="project.beGithub" style="padding: 0">
+					<p>
+						Backend GitHub:
+						<a :href="project.beGithub">{{ project.beGithub }}</a>
+					</p>
+				</div>
+				<div>
+					<p>
+						Hosted: <a :href="project.hosted">{{ project.hosted }}</a>
+					</p>
+				</div>
 			</li>
-			<li>
-				<p>Hosted: <a :href="project.hosted"> {{ project.hosted }}</a>
-				</p>
-			</li>
-		</li>
-</div>
-</div>
+		</ul>
+		<!-- </div> -->
+	</div>
 </template>
