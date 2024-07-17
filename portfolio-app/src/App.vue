23f000d60adbd886b111c9251c8e0fcd75f72281
Gareth A <garethjaustin@gmail.com>
2024-07-17 15:42:32 +0100

    adds hightlighting for the nav to indicate current page

--- portfolio-app/src/App.vue
+++ portfolio-app/src/App.vue
@@ -11,21 +11,31 @@ const routes = {
 	"/projects": Projects,
 };
 
-const currentPath = ref(window.location.hash);
+const currentPath = ref(window.location.pathname);
 
-window.addEventListener("hashchange", () => {
-	currentPath.value = window.location.hash;
+window.addEventListener("popstate", () => {
+	currentPath.value = window.location.pathname;
 });
+
+const navigate = (path) => {
+	window.history.pushState({}, path, path);
+	currentPath.value = path;
+};
 
 const currentView = computed(() => {
-	return routes[currentPath.value.slice(1) || "/"] || NotFound;
+	return routes[currentPath.value] || NotFound;
 });
+
+const applyHighlight = (path) => {
+	return currentPath.value === path ? "highlight" : "";
+};
 </script>
 
 <template>
 	<header>
-		<a href="#/">Home</a> | <a href="#/about">About</a> |
-		<a href="#/projects">Projects</a>
+		<a :class="applyHighlight('/')" href="/">Home</a> |
+		<a :class="applyHighlight('/about')" href="/about">About</a> |
+		<a :class="applyHighlight('/projects')" href="/projects">Projects</a>
 	</header>
 	<main>
 		<component :is="currentView" />
