a1cf33d4bca0c03ca7be1598e3026093812858b6
Gareth A <garethjaustin@gmail.com>
2024-07-31 20:57:40 +0100

    adds ruled line to footer

--- portfolio-app/src/App.vue
+++ portfolio-app/src/App.vue
@@ -1,29 +1,35 @@
 <template>
-  <header>
-    <router-link :class="applyHighlight('/')" to="/">Home</router-link> |
-    <router-link :class="applyHighlight('/about')" to="/about">About</router-link> |
-    <router-link :class="applyHighlight('/projects')" to="/projects">Projects</router-link>
-  </header>
-  <main>
-    <router-view></router-view>
-  </main>
-  <footer>
-    <p>Made by Gareth Austin 2024</p>
-  </footer>
+	<header>
+		<router-link :class="applyHighlight('/')" to="/">Home</router-link> |
+		<router-link :class="applyHighlight('/about')" to="/about"
+			>About</router-link
+		>
+		|
+		<router-link :class="applyHighlight('/projects')" to="/projects"
+			>Projects</router-link
+		>
+	</header>
+	<main>
+		<router-view></router-view>
+	</main>
+	<footer>
+		<hr />
+		<p style="margin: 0">Made by Gareth Austin 2024</p>
+	</footer>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
+import { useRoute } from "vue-router";
 
 const route = useRoute();
 
 const applyHighlight = (path) => {
-  return route.path === path ? 'highlight' : '';
+	return route.path === path ? "highlight" : "";
 };
 </script>
 
 <style>
 .highlight {
-  font-weight: bold;
+	font-weight: bold;
 }
 </style>
